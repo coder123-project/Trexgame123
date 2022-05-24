20d5c812d05dc2c1914d6aa72f240ef644900862
coder123-project <83872425+coder123-project@users.noreply.github.com>
2022-05-25 00:41:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+var trex ,trex_running;
+
+
+
+function preload(){
+  
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+
+
+}
+
+
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex=createSprite(50,160,10,50)
+ trex.addAnimation("running",trex_running)
+trex.scale=0.5
+
+
+
+
+}
+
+
+
+function draw(){
+  
+ background("white");
+
+if(keyDown("space"))
+{
+
+  trex.velocityY=-10
+}
+
+  trex.velocityY=trex.velocityY+0.5
+  
+drawSprites()
+}
